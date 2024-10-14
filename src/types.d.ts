ec97ba158c4b297835f4c603dfc23ef473445694
ChronicStone <thao@agorastore.fr>
2024-10-14 18:01:29 +0200

    feat(types): add basic type definitions

---
+++ src/types.d.ts
@@ -0,0 +1,19 @@
+declare class HakunaTupu {
+  constructor(options?: {
+    removeEmptyArrays?: boolean;
+    removeEmptyObjects?: boolean;
+    removeEmptyStrings?: boolean;
+    removeUndefined?: boolean;
+    removeNull?: boolean;
+  });
+  
+  options: {
+    removeEmptyArrays: boolean;
+    removeEmptyObjects: boolean;
+    removeEmptyStrings: boolean;
+    removeUndefined: boolean;
+    removeNull: boolean;
+  };
+  
+  removeEmpty<T extends object> (obj: T): T;
+}
